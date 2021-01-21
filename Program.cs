5dce0b3480e700996b86a6d2c1a69c55fc518719
yagox10 <yagocgomes3@yahoo.com.br>
2021-01-21 19:36:44 -0300

    teste

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace ServerApplication
+{
+    class Program
+    {
+        private static Thread threadConsole;
+        private static bool consoleRunning;
+
+        static void Main(string[] args)
+        {
+            threadConsole = new Thread(new ThreadStart(consoleThread));
+            threadConsole.Start();
+        }
+
+        private static void consoleThread()
+        {
+            string line;
+            consoleRunning = true;
+
+            while(consoleRunning)
+            {
+                line = Console.ReadLine();
+                
+                if(String.IsNullOrWhiteSpace(line))
+                {
+                    consoleRunning = false;
+                    return;
+                }
+                else
+                {
+
+                }
+
+
+            }
+        }
+
+    }
+
+}
---
+++ network.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ServerApplication
+{
+    class Network
+    {
+        public TcpListener ServerSocket;
+        public static Network instance = new Network();
+        public static Client[] Clients = new Client[100];
+
+        public void ServerStart()
+        {
+            for(int i = 1; i < 100; i++)
+            {
+                Clients[i] = new Client();
+            }
+
+            ServerSocket = new TcpListener(IPAddress.Any, 5500);
+            ServerSocket.Start();
+            ServerSocket.BeginAcceptTcpClient();
+        }
+
+        void OnClientConnect(IAsyncResult result)
+        {
+            TcpClient client = ServerSocket.EndAcceptTcpClient(result);
+            client.NoDelay = false;
+            ServerSocket.BeginAcceptTcpClient(OnClientConnect, null);
+
+
+        }
+    }
+}
